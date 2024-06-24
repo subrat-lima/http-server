8759b19254021d1510e21e15b0fe0bb1b56baa8a
subrat lima <subrat.k.lima@protonmail.com>
2024-06-24 20:28:26 +0530

    added support for html, css, js and common img files

--- src/app.py
+++ src/app.py
@@ -1,24 +1,41 @@
 import re
 import os
+import gzip
 import socket
 import argparse
 
 HOST = "localhost"
-PORT = 4221
 
 STATUS_CODES = {200: "OK", 404: "Not Found"}
 
+SUPPORTED_ENCODING_FORMATS = ["gzip"]
+FILE_TYPES = {
+    "html": "text/html",
+    "css": "text/css",
+    "js": "text/javascript",
+    "png": "image/png",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "webp": "image/webp",
+    "svg": "image/svg+xml",
+    "mp3": "audio/mpeg",
+    "mp4": "video/mp4",
+    "aac": "audio/aac",
+}
 
-def get_directory():
+
+def get_options():
     directory = os.getcwd()
-    parser = argparse.ArgumentParser(description="selects directory to scan")
+    port = 4221
+    parser = argparse.ArgumentParser(description="options to configure http server")
     parser.add_argument("--directory", help="selects directory to scan")
+    parser.add_argument("--port", help="selects the port")
     args = parser.parse_args()
-    print(f"directory: {args.directory}")
     if args.directory is not None:
         directory = args.directory
-        print(f"directory set to {directory}")
-    return directory
+    if args.port is not None:
+        port = int(args.port)
+    return {"directory": directory, "port": port}
 
 
 def parse_request(data_packet):
@@ -27,20 +44,74 @@ def parse_request(data_packet):
     protocol = re.search(r"(?P<protocol>HTTP/\d\.\d)", data_packet).group("protocol")
     host = re.search(r"Host:\s(?P<host>\w+:\d+)", data_packet).group("host")
 
+    accept_encoding = []
+    if "Accept-Encoding" in data_packet:
+        accept_encoding_set = set(
+            re.search(
+                r"Accept-Encoding:\s(?P<accept_encoding>([a-zA-Z0-9-]+(,\s)?)+)",
+                data_packet,
+            )
+            .group("accept_encoding")
+            .split(", ")
+        )
+        for encoding in accept_encoding_set:
+            if encoding is SUPPORTED_ENCODING_FORMATS:
+                accept_encoding.append(encoding)
+        if len(accept_encoding) > 0:
+            accept_encoding = ", ".join(accept_encoding)
+
     body = None
     request = {
-        "headers": {"method": method, "path": path, "protocol": protocol, "host": host},
+        "headers": {
+            "method": method,
+            "path": path,
+            "protocol": protocol,
+            "host": host,
+            "accept_encoding": accept_encoding,
+        },
         "body": body,
     }
     return request
 
 
-def generate_response(request, status_code, body):
-    header = f"{request['headers']['protocol']} {status_code} {STATUS_CODES[status_code]}\r\n\r\n"
-    header = header.encode()
-    body = body.encode()
-    response = header + body
-    return response
+def generate_response(request, response):
+    headers = f"{request['headers']['protocol']} {response['headers']['status_code']} {STATUS_CODES[response['headers']['status_code']]}\r\n"
+    if "content_type" in response["headers"]:
+        headers += f"Content-Type: {response['headers']['content_type']}\r\n"
+        headers += f"Content-Length: {response['headers']['content_length']}\r\n"
+    if "accept_encoding" in response["headers"]:
+        headers += f"Content-Encoding: {response['headers']['accept_encoding']}\r\n"
+    headers += "\r\n"
+    response_packet = b"".join([headers.encode(), response["body"]])
+    print("response: ")
+    print(response_packet)
+    return response_packet
+
+
+def resolve_request(request, response):
+    directory = get_options()["directory"]
+    file_path = request["headers"]["path"]
+    if file_path == "/":
+        file_path = "index.html"
+    else:
+        file_path = file_path[1:]
+
+    response["headers"] = {"status_code": 404}
+    response["body"] = b""
+    file_type = file_path.split(".")[-1]
+    file_name = os.path.join(directory, file_path)
+    if os.path.exists(file_name):
+        with open(file_name) as f:
+            response["body"] = f.read()
+            if "gzip" in request["headers"]["accept_encoding"]:
+                response["body"] = gzip.compress(bytes(body, "utf-8"))
+                response["headers"]["accept_encoding"] = request["headers"][
+                    "accept_encoding"
+                ]
+            response["headers"]["content_length"] = len(response["body"])
+            response["headers"]["content_type"] = FILE_TYPES[file_type]
+            response["headers"]["status_code"] = 200
+            response["body"] = response["body"].encode()
 
 
 def handle_connection(connection):
@@ -48,29 +119,21 @@ def handle_connection(connection):
     print(data_packet)
     request = parse_request(data_packet.decode())
 
-    body = ""
-    if request["headers"]["path"] == "/":
-        status_code = 200
-    else:
-        directory = get_directory()
-        print(f"directory: {directory}")
-        file_name = os.path.join(directory, request["headers"]["path"][1:])
-        print(file_name)
-        if os.path.exists(file_name):
-            print(f"file_found")
-            status_code = 200
-            with open(file_name) as f:
-                body = f.read()
-        else:
-            status_code = 404
+    response = {}
+    resolve_request(request, response)
+    print("resolved_request: ")
+    print(response)
 
-    response = generate_response(request, status_code, body)
-    connection.sendall(response)
+    response_packet_data = generate_response(request, response)
+    print(f"response_packet_data:")
+    print(response_packet_data)
+    connection.sendall(response_packet_data)
 
 
 def main():
-    with socket.create_server((HOST, PORT), reuse_port=True) as server_socket:
-        print(f"server running at http://{HOST}:{PORT}")
+    port = get_options()["port"]
+    with socket.create_server((HOST, port), reuse_port=True) as server_socket:
+        print(f"server running at http://{HOST}:{port}")
         while True:
             connection, addr = server_socket.accept()
             with connection:
